d3854f9384380b87e43a1c94742b97445b02afb0
montassar1998 <{montassarriahi@aol.com}>
2022-11-10 08:32:30 -0500

    implemented read an account from service

--- service/routes.py
+++ service/routes.py
@@ -70,6 +70,14 @@ def create_accounts():
 
 # ... place you code here to READ an account ...
 
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accoounts(account_id):
+    app.logger.info("Request to read an Account with id: %s",account_id)
+    account = Account.find(account_id)
+    if not account: 
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    
+    return account.serialize(), status_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
